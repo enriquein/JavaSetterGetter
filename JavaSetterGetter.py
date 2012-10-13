32550dc5ce04b55b3ae1746fcd2da54e7474fa29
Enrique Ramirez <enriquein@gmail.com>
2012-10-12 21:50:31 -0400

    Trying to get something working.

--- JavaSetterGetter.py
+++ JavaSetterGetter.py
@@ -1,22 +1,33 @@
-import sublime, sublime_plugin
+import sublime, sublime_plugin, indentation
 
 class JavaSetterGetterCommand(sublime_plugin.TextCommand):
     def run(self, edit):
         self.view.run_command("expand_selection", {"to": "line"}) 
 
-        edit = self.view.begin_edit('java_setter_getter')
+       
         sels = self.view.sel()
 
         end_position = 0
         all_text = []
+        properties = []
 
         for sel in sels:
             if sel.end > end_position:
                 end_position = sel.end()
-                all_text.append(self.view.substr(sel))
+                all_text = self.view.substr(sel).split('\n')
+                print all_text
 
-        print all_text
+        for line in all_text:
+            if len(line) > 0:
+                plain_line = line.split()
+                if len(plain_line) != 3:
+                    sublime.error_message("You probably have some syntax issues. Check your code.")
+                    return
+            
 
-        self.view.insert(edit, end_position, "\nSuck it!")
 
-        self.view.end_edit(edit)
+
+        #edit = self.view.begin_edit('java_setter_getter')
+        #self.view.insert(edit, end_position, "\nSuck it!")
+
+        #self.view.end_edit(edit)
